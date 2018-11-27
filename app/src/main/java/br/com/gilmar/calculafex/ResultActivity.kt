c83e3227401f2e7027c03f07f36d5564ae33dc64
Logon Aluno <logonrmlocal@fiap.com.br>
2018-11-26 23:15:12 -0200

    projeto 17mob

---
+++ app/src/main/java/br/com/gilmar/calculafex/ResultActivity.kt
@@ -0,0 +1,12 @@
+package br.com.gilmar.calculafex
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
